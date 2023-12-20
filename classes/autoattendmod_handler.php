cb7e65c65c698fab81c7bd7aa1103d01a6adc9fe
root <iseki>
2023-12-20 12:06:02 +0900

    1st

---
+++ classes/autoattendmod_handler.php
@@ -0,0 +1,17 @@
+﻿<?php
+
+defined('MOODLE_INTERNAL') || die();
+
+#require_once(dirname(__FILE__).'/../locallib.php');
+#require_once(dirname(__FILE__).'/../../../lib/sessionlib.php');
+#require_once(dirname(__FILE__).'/../../../lib/datalib.php');
+
+
+class autoattendmod_handler 
+{
+
+    public static function get_attendances_handler($userid) 
+    {
+        return "MORE HELL World!!!";
+    }
+}
---
+++ version.php
@@ -0,0 +1,25 @@
+﻿<?php 
+
+/////////////////////////////////////////////////////////////////////////////////
+///  Code fragment to define the version of autoattendmod
+///  This fragment is called by moodle_needs_upgrading() and /admin/index.php
+/////////////////////////////////////////////////////////////////////////////////
+
+defined('MOODLE_INTERNAL') || die();
+
+//
+$plugin->requires  = 2012120300;          // Moodle 2.4
+$plugin->component = 'mod_autoattendmod';
+$plugin->cron      = 300;
+$plugin->maturity  = MATURITY_STABLE;
+
+$plugin->release   = '2.6.5';
+
+$plugin->version   = 2023112800;    // Match to block version
+//$plugin->version = 2023032300;    // Local hostname
+//$plugin->version = 2022051300;    // remove add_to_log
+//$plugin->version = 2019082106;    // v2.6.0 for moodle-3.7.x
+//$plugin->version = 2019081700;    // old excel version
+//$plugin->version = 2016011200;    // backup
+//$plugin->version = 2016031900;    // email function
+
