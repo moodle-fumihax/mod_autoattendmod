28201b94589a21a9b857700c8730b7642655823e
iseki <root@jogeid.net>
2026-09-02 22:32:41 +0900

    2.7.8

--- version.php
+++ version.php
@@ -13,9 +13,10 @@ $plugin->component = 'mod_autoattendmod';
 $plugin->cron      = 300;
 $plugin->maturity  = MATURITY_STABLE;
 
-$plugin->release   = '2.7.7';
+$plugin->release   = '2.7.8';
 
-$plugin->version   = 2025122200;    // no change. Adjusting to block_autoattend
+$plugin->version   = 2026090200;    // no change. Adjusting to block_autoattend
+//$plugin->version = 2025122200;    // no change. Adjusting to block_autoattend
 //$plugin->version = 2025071401;    // delete autoattend_email2teachers_key()
 //$plugin->version = 2025071400;    // fix autoattendmod_grade_item_update()
 //$plugin->version = 2025020304;    // Match to block version
